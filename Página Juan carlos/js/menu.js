044215558e8695a2451688f64df63afdc675b1a5
Xander07 <japaniaguab@unal.edu.co>
2019-03-22 21:15:01 -0500

    diseño definido

--- Página Juan carlos/js/menu.js
+++
@@ -1,5 +0,0 @@
-var btnMenu = document.getElementById('btn-menu');
-var nave = document.getElementById('nave');
-btnMenu.addEventListener('click', function(){
-  nave.classList.toggle('mostrar');
-});
