f06130a162b2f65b1f742d3b4be6a3273df351fd
Xander07 <japaniaguab@unal.edu.co>
2019-03-22 23:22:35 -0500

    Diseño página inicial

---
+++ configuraciones/conexion.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost";
+$user = "final";
+$pass = "final";
+$DB = "final";
+$conn = mysqli_connect($host, $user, $pass, $DB) or die("Error al conectar a la DB " . mysqli_error($link));
+?>
