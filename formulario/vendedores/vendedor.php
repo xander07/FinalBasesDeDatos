1a9bf9ba179f6a0dc6dca6c17c8149f56513e31b
Xander07 <japaniaguab@unal.edu.co>
2019-03-25 12:38:55 -0500

    cambio css

--- formulario/vendedores/vendedor.php
+++ formulario/vendedores/vendedor.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <title>Trabajo Final Base de Datos</title>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="../css/estilos.css">
     <link href="https://file.myfontastic.com/RkCWtGmXx2KTJLHJqWTNtS/icons.css" rel="stylesheet">
 </head>
-<body bgcolor ="black" background="img/fondo.png">
+<body bgcolor ="black" background="../img/fondo.png">
    <header class="header">
        <div class="contenedor">
            <span class="icon-menu" id="btn-menu"></span>
@@ -23,29 +23,44 @@
   <div class="banner">
       <img src="img/bannerv.png" alt="" class="banner__v__img">
       <div class="contenedor">
-          <h2 class="banner__titulo"> Bienvenido al menú de vendedores</h2>
-          <p class="banner__txt">Aquí podrás registrar y borrar vendedores.</p>
+          <h2 class="banner__titulo"> Bienvenido al menú de registro de vendedores</h2>
+          <p class="banner__txt">Aquí podrás registrar vendedores.</p>
       </div>
   </div>
 
-  <main class="main">
-    <div class="contenedor">
-      <form action="insert_v.php" class="form-group" method="post">
+  <main class="main__f">
+    <div class="formulario">
+      <form action="#" class="formulario" method="post">
         <div class="form-group">
-          <label for="cedula">Cedula</label>
-          <input type="text" name="cedula" id="cedula" class="form-control">
+          <label for="id">Numero de identificacion</label>
+          <input type="text" name="id" id="id" class="form-control" required>
         </div>
         <div class="form-group">
           <label for="">Nombre</label>
-          <input type="text" name="name" id="name" class="form-control">
+          <input type="text" name="name" id="name" class="form-control" required>
         </div>
         <div class="form-group">
-          <label for="">Direccion</label>
-          <input type="text" name="direccion" id="direccion" class="form-control">
+          <label for="">Fecha de Nacimiento</label>
+          <input type="date" name="fNacimiento" id="FNacimiento" class="form-control" required>
         </div>
         <div class="form-group">
-          <label for="">Telefono</label>
-          <input type="text" name="telefono" id="telefono" class="form-control">
+          <label for="">email</label>
+          <input type="email" name="email" id="email" class="form-control" required>
+        </div>
+        <div class="form-group">
+          <label for="">contrasena</label>
+          <input type="password" name="password" id="password" class="form-control" required>
+        </div>
+        <div class="form-group">
+          <label for="">Pais</label>
+          <input type="text" name="pais" id="pais" class="form-control" required>
+        </div>
+        <div class="form-group">
+          <label for="">Alcance</label>
+          <select name ="alcance" required>
+            <option>Nacional</option>
+            <option>Internacional</option>
+          </select>
         </div>
         <div class="form-group">
           <input type="submit" class="btn btn-primary" value="insertar">
